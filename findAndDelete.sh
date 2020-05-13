46bd19e768a3e9621a7ccff1bf8ec5d00fee710c
cykz <cykz@cykz.com>
2020-05-13 23:49:28 +0200

    added information about amount of deleted files

--- findAndDelete.sh
+++ findAndDelete.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
 DIRECTORY = /var/tmp
+COUNTER = $(find . -name "*.tmp" -mindepth 1 -mtime +1 -exec {} \; | wc -l)
 
 cd $DIRECTORY
 find . -name "*.tmp" -mindepth 1 -mtime +1 -delete #or we can use -exec rm -rf {} \;
+echo "$COUNTER files have been deleted"
